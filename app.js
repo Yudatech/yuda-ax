a0b7be467832de5148b84dddb459694ce04308a9
Hui Su <yudasuhui@hotmail.com>
2018-05-11 23:54:43 +0200

    create new repository
    Finish cookies bar and nav bar

---
+++ app.js
@@ -0,0 +1,22 @@
+// Require
+const express = require('express');
+const compression = require('compression');
+
+// Create express server
+const app = express();
+
+// Express middleware
+app.use(compression());
+app.use(express.static('www'));
+
+// Serve the index.html page on every request that
+// doesn't have a file extension in its url
+// (so that single page apps work on page reload)
+app.get(/^[^\.]*$/, (req, res) => {
+  res.sendFile(__dirname + '/www/index.html');
+});
+
+// Start server
+app.listen(3000, () =>
+  console.log('Webserver listening on port 3000')
+);
